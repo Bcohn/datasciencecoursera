9bf52aeca4f0dc90f0de3bd512e0bb1317787d91
Brent Cohn <brent@Puppis.local>
2014-04-27 13:27:55 -0400

    project complete

---
+++ README.md
@@ -0,0 +1,10 @@
+#Getting and Cleaning Data Peer Assessment Project
+
+##Contents:
+1. README.md
+2. run_analysis.R
+
+##File Description:
+
+* run_analysis.R is an R script that loads four separate datasets, labels the variables, and then merges them. after merging the data, the script searches for variables that have mean or std (standard deviation) in their titles, filters out these variables, and creates a new dataset called "tidyData"
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,38 @@
 
+#load test data
 testData <- read.table("/Users/brent/datasciencecoursera/getAndClean/test/X_test.txt")
-testSubjects <- read.table("/Users/brent/datasciencecoursera/getAndClean/test/y_test.txt")
-completeTest <- cbind(testSubjects,testData)
+testActivity <- read.table("/Users/brent/datasciencecoursera/getAndClean/test/y_test.txt")
+#merge test data
+completeTest <- cbind(testActivity,testData)
 
-
+#load training data
 trainData <- read.table("/Users/brent/datasciencecoursera/getAndClean/train/X_train.txt")
-trainSubjects <- read.table("/Users/brent/datasciencecoursera/getAndClean/train/y_train.txt")
-completeTrain <- cbind(trainSubjects,trainData)
+trainActivity <- read.table("/Users/brent/datasciencecoursera/getAndClean/train/y_train.txt")
+#merge training data
+completeTrain <- cbind(trainActivity,trainData)
 
-
+#load labels
 Labels <- read.table("/Users/brent/datasciencecoursera/getAndClean/features.txt")
 
+#label data
 colnames(testData) <- testLabels$V2
 colnames(trainData) <- testLabels$V2
 
+#label activities
 names(completeTest)[1] <- "activity"
 names(completeTrain)[1] <- "activity"
 
+#test tomake sure variable names are equivalent in training & test for
+#the variablse we care about
 grep("mean",names(completeTrain), value=T)==grep("mean",names(completeTest))
 
+#pull out mean and standard deviation variables
 meanVars <- grep("mean",names(completeTrain), value=T)
 stdVars <- grep("std",names(completeTrain), value=T)
+#merge mean and standard deviation variables
 varsToKeep <- c(meanVars,stdVars, "activity")
 
+#filter data for only mean, std dev, and activity name variables
 keepTrain <- completeTrain[,varsToKeep]
 keepTest <- completeTest[,varsToKeep]
 length(varsToKeep)==dim(keepTrain)[2]
@@ -30,9 +40,13 @@ length(varsToKeep)==dim(keepTest)[2]
 
 almostTidyData <- rbind(keepTrain,keepTest)
 
+#label activities
 activityLabel <- read.table("/Users/brent/datasciencecoursera/getAndClean/activity_labels.txt")
 names(activityLabel) <- c("activity","activityName")
- 
-tidyData <- merge(x=almostTidyData,y=activityLabel,by="activity")
 
+#label activities
+tidyData <- merge(x=almostTidyData,y=activityLabel,by="activity")
+#drop numeric activity column
 tidyData <- tidyData[-1]
+
+write.table(tidyData,file="tidyData.txt")
